556813bfc2407a40bb64d8587da6b6bf0d8a99e1
Andrew Imrie <andrewimrie99@gmail.com>
2019-11-20 15:47:36 -0500

    jsoup + DDS external calls priliminary skeleton

--- src/main/java/CrawlerController.java
+++ src/main/java/CrawlerController.java
@@ -1,3 +1,7 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import spark.Request;
 import spark.Response;
 
@@ -28,5 +32,44 @@ public class CrawlerController {
     // Crawl the URL
 
     // Set the response body
+  }
+
+  /**
+   * Function to call jsoup and return the raw HTML of the given url
+   *
+   * @param url the URL that will be crawled for its HTML
+   * @return the raw HTML in a string
+   */
+  private String getHTML(String url){
+    // Call jsoup to get the HTML
+    // Return value
+  }
+  /**
+   * Function to call jsoup and return a vector of links from the page
+   *
+   * @param url the URL that will be crawled for its HTML
+   * @return a list of links
+   */
+  private List<String> getLinks(String url){
+    // Call jsoup to get the links
+    // Return value
+  }
+
+  /**
+   * Function to facilitate pushing info to Document Data Store
+   *
+   * @param res the JSON response to be pushed to DDS
+   */
+  private void pushToDDS(Response res){
+
+  }
+
+  /**
+   * Function to facilitate pulling info from Document Data Store
+   *
+   *
+   */
+  private void pullFromDDS(){
+
   }
 }
