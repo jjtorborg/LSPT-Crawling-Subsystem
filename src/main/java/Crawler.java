568bb6e47c3ed7749126509bb65712aa2d9487b1
Andrew Imrie <andrewimrie99@gmail.com>
2019-11-20 19:42:50 -0500

    im leaving in 15 minutes -Rambo

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 public class Crawler {
 
-<<<<<<< HEAD
     public Crawler() {
     }
 
@@ -59,57 +58,4 @@ public class Crawler {
         // make a PUT request to DDS containing the URL, full html document, out-links (anchors), time
         // crawled, and recrawl time.
     }
-=======
-  public Crawler() {}
-  
-  /**
-     * Gets content and links present on input url
-     * and sends them to LA and DDS
-     * 
-     * @param url
-     */
-  public void crawlUrl(String url) {
-    // Perform HTTP GET on input url
-
-    // Parse text content and contained urls from input webpage
-
-  /**
-   * Function to call jsoup and return the raw HTML of the given url
-   *
-   * @param url the URL that will be crawled for its HTML
-   * @return the raw HTML in a string
-   */
-  private String getHTML(String url) {
-    // Call jsoup to get the HTML
-    // Return value
-    return "";
-  }
-
-  /**
-   * Function to call jsoup and return a vector of links from the page
-   *
-   * @param url the URL that will be crawled for its HTML
-   * @return a list of links
-   */
-  private List<String> getLinks(String url) {
-    // Call jsoup to get the links
-    // Return value
-    return null;
-  }
-
-  /**
-   * Function to facilitate pushing info to Document Data Store
-   *
-   * @param res the JSON response to be pushed to DDS
-   */
-  private void pushToDDS(Response res) {
-    // If a URL is crawled that contains a doc that should not be a search result,
-    // make a DELETE request to DDS which includes the crawled URL
-
-    // If the crawled URL contains a doc that could be a search result,
-    // make a PUT request to DDS containing the URL, full html document, out-links (anchors), time
-    // crawled, and recrawl time.
-  }
->>>>>>> baf0f8f9c10750ee31afa19099ab5b9b17c85938
-
 }
